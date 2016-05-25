dd99de1ffef73e59e37163842c784576012217f3
malgorzataolak <m.olak@outlook.com>
2016-05-26 01:07:29 +0200

    Zmiana szablonu, dodanie nowego modelu, modyfikacja jego pol, dodanie statycznych plikow - css i obrazki

--- blog/models.py
+++ blog/models.py
@@ -15,3 +15,20 @@ class Post(models.Model):
 
 	def __str__(self):
 		return self.title
+
+
+class ArtistInfo(models.Model):
+	artist_name=models.CharField(max_length=200)
+	add_photo=models.ImageField(upload_to='static/images')
+	birth_date=models.DateField()
+	country=models.CharField(max_length=200)
+	instruments=models.CharField(max_length=200)
+	music_genre=models.CharField(max_length=200)
+	record_label=models.CharField(max_length=200)
+	group=models.CharField(max_length=200)
+
+	def addTo(self):
+		self.save()
+
+	def __str__(self):
+		return self.artist_name
