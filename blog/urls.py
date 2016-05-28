5e931a638a932ec332035b9672e43f156b3670e7
malgorzataolak <m.olak@outlook.com>
2016-05-28 15:46:50 +0200

    Dodanie formularzy do tworzenia postow i ich edycji prosto ze strony. Tylko dla zalogowanego uzytkownika.

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = patterns('',
     url(r'^$', views.post_list, name='index'),
     url(r'^posts/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name="post_new"),
     
 )
