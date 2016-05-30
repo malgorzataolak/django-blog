fe25ca9701e2e1fa16e625c7bd8d78fade28636e
malgorzataolak <m.olak@outlook.com>
2016-05-30 10:30:12 +0200

    Dodanie modulu komentarzy do postow, stworzenie formularza do wprowadzania komentarzy oraz mozliwosci zatwierdzania lub odrzucania

--- blog/models.py
+++ blog/models.py
@@ -13,6 +13,9 @@ class Post(models.Model):
 		self.published_date=timezone.now()
 		self.save()
 
+	def approved_comments(self):
+		return self.comments.filter(approved_comment=True)
+
 	def __str__(self):
 		return self.title
 
@@ -33,4 +36,16 @@ class ArtistInfo(models.Model):
 	def __str__(self):
 		return self.artist_name
 
+class Comment(models.Model):
+	post=models.ForeignKey('blog.Post', related_name='comments')
+	author=models.CharField(max_length=200)
+	text=models.TextField()
+	created_date=models.DateTimeField(default=timezone.now)
+	approved_comment=models.BooleanField(default=False)
 
+	def approve(self):
+		self.approved_comment=True
+		self.save()
+
+	def __str__(self):
+		return self.text
--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,7 @@ urlpatterns = patterns('',
     url(r'^posts/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name="post_new"),
-    
+    url(r'^post/(?P<pk>\d+)/comment/$', views.add_comment, name="add_comment"),
+    url(r'^comment/(?P<pk>\d+)/approve/$', views.comment_approve, name="comment_approve"),
+    url(r'^comment/(?P<pk>\d+)/remove/$', views.comment_remove, name="comment_remove"),
 )
